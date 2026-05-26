7a54bd6e038b4c904e21dc5eec851fb917c81e73
Tejesh Gangari <tgdevelops@gmail.com>
2026-05-26 17:12:01 +0530

    Initial Commit

---
+++ little-scholar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  little-scholar
+//
+//  Created by Tejesh on 26/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ little-scholar/little_scholarApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_scholarApp.swift
+//  little-scholar
+//
+//  Created by Tejesh on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct little_scholarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
